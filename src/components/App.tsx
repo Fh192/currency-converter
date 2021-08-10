06f910ff1ee7ad9cf6254052bdbd7d76759b7f42
Kirill Shevchenko <stranger.net3@gmail.com>
2021-08-10 22:14:34 +0400

    Add tab box component

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TabBox from './TabBox/TabBox';
+
+const App: React.FC = () => {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <TabBox />
+      </div>
+    </div>
+  );
+};
+
+export default App;
