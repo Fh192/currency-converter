02f529f70f1bd6b197e0e43fda826710a0e1c6ba
Kirill Shevchenko <stranger.net3@gmail.com>
2021-08-10 22:11:56 +0400

    Update api types

--- src/types/apiTypes.ts
+++ src/types/apiTypes.ts
@@ -1,13 +1,16 @@
 export interface LatestRateResponse {
   status: boolean;
-  code: 200 | 113 | 101;
+  code: 200 | 213 | 113 | 101;
   msg:
     | 'Successfully'
-    | 'Sorry, Something wrong, or an invalid value. Please try again or check your required parameters.';
+    | 'Sorry, Something wrong, or an invalid value. Please try again or check your required parameters.'
+    | 'Access block for you, You have reached maximum 3 limit per minute in free account, Please stop extra hits or upgrade your account. Restriction remove after 1 minute.'
+    | 'Access block for you, You have reached maximum 3 limit per minute in free account, Please stop extra hits or upgrade your account. Restriction remove after 1 minute.';
   response: [
     {
       o: string; //rate
       cp: string; //Change in percentage
+      tm: string; // When update last time (UTC)
     }
   ];
 }
